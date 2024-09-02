02f1da716ca59b8b01db7bdda6ff8fe4ed19df24
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-02 22:09:26 +0700

    Add Tipe Data.kt

---
+++ src/main/kotlin/Tipe Data.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var minByte: Byte = -128
+    var maxByte: Byte = 127//8bit
+    val minShort: Short = -32768
+    val maxShort: Short = 32767 //16bit
+    val minInt: Int = -2147483648
+    val maxInt: Int = -2147483648//32bit
+    val minLong: Long = -9223372036854775807
+    val maxLong: Long = 9223372036854775807 //64bit
+
+    println("minByte:" + minByte)
+    println("maxByte:" + minByte)
+    println("minShort:" + minShort)
+    println("maxShort:" + maxShort)
+    println("minInt:" + minInt)
+    println("maxInt:" + maxInt)
+    println("minLong:" + minLong)
+    println("maxLong:" + maxLong)
+}
