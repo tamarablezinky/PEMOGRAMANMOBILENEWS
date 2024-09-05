dfeccf2d7c6adb8ac9eb951934bfa9b330961d50
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-05 09:52:52 +0700

    Add Constants Literasi.kt

---
+++ src/main/kotlin/Constants Literal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var floatLiteral: Float = 178.95F //literal float
+    println("contoh literal float : " + floatLiteral)
+    var oneBillion: Long = 100000000L //Literal Long
+    println("contoh literasi long: " + oneBillion)
+    val valHeksa = 0x0F //LITERAL HEKSADESIMAL DIAWALI DENGAN 0X
+    println("contoh literasi Heksadesimal: " + valHeksa)
+    val valBinary = 0b0001011 //literal binary diawali dengan 0b
+    println("contoh literasi Binary: " + valBinary)
+    val notasiKonvensional = 123.5e10
+    println("contoh literasi konvensional floating point Number :" + notasiKonvensional)
+}
