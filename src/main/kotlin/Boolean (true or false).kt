43a65525553ecdad9d4e4f2091d3fcafa137b013
tamarablezinky <tamarablezinky05@gmail.com>
2024-09-05 10:40:35 +0700

    Add String (Array of Character).kt

--- src/main/kotlin/Boolean (true or false).kt
+++ src/main/kotlin/Boolean (true or false).kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>) {
-    var varBool : Boolean = true
+    var varBool: Boolean = true
 
-    if(varBool)
+    if (varBool)
         println("varBool bernilai " + varBool)
+    var varInt : Int = 1
+
+    if(varInt)
+        println("varInt bernilai " + varInt)
 }
---
+++ src/main/kotlin/String (Array of Character).kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val myName: String = "Tamara Blezinky"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
